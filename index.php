e1652d70679df8a5b6fb56e62dda944006a53c4e
Sophiequehen <sophie.quehen@gmail.com>
2019-09-30 16:43:01 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,434 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+	<title>Liberty'Time</title>
+	<!-- <link rel="shortcut icon" href="img/favicon.png" />
+	<link rel="icon" type="image/x-icon" href="img/favicon.png" />
+	<link rel="icon" type="image/png" href="img/favicon.png" /> -->
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700&display=swap" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,700&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/mediaqueries.css">
+	<link rel="stylesheet" href="css/animations.css">
+</head>
+<body>
+	<section id="page-burger" style="display: none">
+		<ul class="nav-burger">
+			<li class="liens-burger"><a href="#welcome">ACCUEIL</a></li>
+			<li class="liens-burger"><a href="#presentation">PRÉSENTATION</a></li>
+			<li class="liens-burger"><a href="#missions">MISSIONS</a></li>
+			<li class="liens-burger"><a href="#tarifs">TARIFS</a></li>
+			<li class="liens-burger"><a href="#contact">CONTACT</a></li>
+		</ul>
+	</section>
+	<header>
+		<div id="sticky-header">
+			<img id="sticky-logo" src="img/svg/logo_sticky.svg" alt="logo Liberty'Time">
+			<ul id="sticky-navbar">
+				<li class=""><a href="#welcome">ACCUEIL</a><div class="underline"></div></li>
+				<li class=""><a href="#presentation">PRÉSENTATION</a><div class="underline"></div></li>
+				<li class=""><a href="#missions">MISSIONS</a><div class="underline"></div></li>
+				<li class=""><a href="#tarifs">TARIFS</a><div class="underline"></div></li>
+				<li class=""><a href="#contact">CONTACT</a><div class="underline"></div></li>
+			</ul>
+			<img class="burger sticky-menu" src="img/svg/menu.svg">
+			<img class="sticky-close" src="img/svg/close.svg">
+			<div id="sticky-icons">
+				<a href=""><img class="icone-facebook" src="img/svg/facebook.svg" alt="icone facebook"></a>
+				<a href="#contact"><img class="icone-email" src="img/svg/enveloppe.svg" alt="icone emal"></a>
+			</div>
+		</div>
+		<div class="header-icons">
+			<a href=""><img class="icone-facebook" src="img/svg/facebook.svg" alt="icone facebook"></a>
+			<a href="#contact"><img class="icone-email" src="img/svg/enveloppe.svg" alt="icone emal"></a>
+		</div>
+		<div id="logo">
+			<img src="img/logo.png" alt="logo Liberty'Time">
+		</div>
+		<ul class="navbar">
+			<li class=""><a href="#welcome">ACCUEIL</a><div class="underline"></div></li>
+			<li class=""><a href="#presentation">PRÉSENTATION</a><div class="underline"></div></li>
+			<li class=""><a href="#missions">MISSIONS</a><div class="underline"></div></li>
+			<li class=""><a href="#tarifs">TARIFS</a><div class="underline"></div></li>
+			<li class=""><a href="#contact">CONTACT</a><div class="underline"></div></li>
+		</ul>
+
+		<div id="navbar-responsive">
+			<img id="sticky-logo" src="img/svg/logo_sticky.svg" alt="logo Liberty'Time">
+			<img class="sticky-close" src="img/svg/close.svg">
+			<img class="burger sticky-menu" src="img/svg/menu.svg">
+		</div>
+
+	</header>
+
+	<div class="content">
+		<section id="welcome">
+			<img id="welcome_img" src="img/welcome.jpg" alt="bureau organisé">
+			<img id="welcome_img_responsive" src="img/welcome_all.jpg" alt="bureau organisé">
+			<div class="title">
+				<h2>Professions libérales, artisans, commerçants...</h2>
+				<h2>Vous travailler souvent 7j/7 et les tâches</h2>
+				<h2>administratives représentent une lourde charge</h2>
+				<h2>dont vous voudriez vous passer ?</h2>
+			</div>
+			<div class="title-responsive">
+				<h2>Professions libérales, artisans, commerçants... Vous travailler souvent 7j/7 et les tâches administratives représentent une lourde charge dont vous voudriez vous passer ?</h2>
+			</div>
+			<div id="content-arrow">
+				<img id="arrow" src="img/svg/arrow.svg" alt="flèche vers le bas">
+			</div>
+		</section>
+
+		<section id="competences">
+			<h3>Devant la complexité administrative, </h3>
+			<h3>une ASSISTANTE INDÉPENDANTE est la solution idéale :</h3>
+			<h3 id="competences-responsive">Devant la complexité administrative, une ASSISTANTE INDÉPENDANTE est la solution idéale :</h3>
+			<ul id="competences-icons">
+				<li><img src="img/svg/gain.svg" alt><p>Gain de temps</p></li>
+				<li><img src="img/svg/flexibility.svg" alt><p>Flexibilité des horaires</p></li>
+				<li><img src="img/svg/embauche.svg" alt><p>Pas de contrainte d'embauche</p></li>
+				<li id="competence-polyvalence"><img src="img/svg/polyvalence.svg" alt><p>Polyvalence des tâches proposées</p></li>
+				<li id="competence-reactivity"><img src="img/svg/reactivity.svg" alt><p>Réactivité à toute épreuve</p></li>
+				<li><img src="img/svg/confidentiality.svg" alt><p>Confidentialité infaillible</p></li>
+				<li id="competence-missions"><img src="img/svg/missions.svg" alt><p>Missions ponctuelles ou régulières</p></li>
+				<li><img src="img/svg/mobility.svg" alt><p>Mobilité et proximité (sur site ou à distance)</p></li>
+			</ul>
+		</section>
+
+		<section id="presentation">
+			<div class="bloc-couleur-un"></div>
+			<img src="img/portrait.jpg" alt="portrait">
+			<div class="bloc-texte">
+				<h2>Perrine Quehen</h2>
+				<div class="hyphen"></div>
+				<p>Grâce à une solide formation (assistanat commercial, gestion d'entreprise), je mets à votre service près de 10 années d'expérience.</p>
+				<p>Ayant exercé au sein de différentes structures privées ou publiques, j'ai cotoyé une grande variété d'activités : aménagement, bâtiment, commerce, industrie, médicale, sociale, ...</p>
+				<p>Celles-ci m'ont permis d'accroître mes compétences afin de vous apporter aujourdh'ui un service performant et adapté.</p>
+			</div>
+			<div class="bloc-couleur-deux"></div>
+			<div id="objectif">
+				<h2>Mon objectif :</h2>
+				<h2>la satisfaction client</h2>
+				<div class="hyphen"></div>
+				<h3>Dynamique, organisée et autonome,</h3>
+				<h3>je serai votre meilleure alliée dans</h3>
+				<h3>votre quotidien professionnel</h3>
+			</div>
+		</section>
+
+		<section id="missions">
+			<h2>MISSIONS</h2>
+			<div class="hyphen"></div>
+			<p class="missions-header">Dans les petites structures comme dans les grands groupes,</p>
+			<p class="missions-header">la productivité passe par une gestion du temps et des coûts optimisés.</p>
+			<p class="missions-header">Le moment est venu de déléguer pour vous concentrer exclusivement sur votre corps de métier.</p>
+			<p class="missions-header">Voici les missions que je vous propose :</p>
+
+			<p id="missions-header-responsive">Dans les petites structures comme dans les grands groupes, la productivité passe par une gestion du temps et des coûts optimisés. Le moment est venu de déléguer pour vous concentrer exclusivement sur votre corps de métier. Voici les missions que je vous propose :</p>
+			<img src="img/bureau.jpg" alt="bureau">
+			<div id="bloc-couleur-trois"></div>
+			<div id="secretariat">
+				<h2>Secrétariat</h2>
+				<div class="hyphen"></div>
+				<p>Mise en forme de documents : courriers, factures, devis, ...</p>
+				<p>Corrections orthographiques</p>
+				<p>Classement et archivage</p>
+				<p>Gestion des appels, du courrier, et des e-mails</p>
+				<p>Accueil physique</p>
+				<p>Prise de messages</p>
+				<p>Prise de notes et comptes rendus</p>
+				<p>Saisie informatique</p>
+				<p>Retranscription audio</p>
+				<p>Numérisations</p>
+				<p>Passation et suivi de commandes</p>
+				<p>Élaboration de tableaux de bord</p>
+				<p>Suivi des paiements, encaissements</p>
+				<p>Relance d'impayés</p>
+			</div>
+			<div id="assistanat">
+				<h2>Assistanat</h2>
+				<div class="hyphen"></div>
+				<p>Traitement des notes de frais</p>
+				<p>Gestion d'un service après-vente</p>
+				<p>interface avec les services administratifs, fiscaux et comptables</p>
+				<p>Négociation de contrats (type assurances, ...)</p>
+				<p>Optimisation des coûts</p>
+				<p>Mise en place d'outils de prospection</p>
+				<p>Création et alimentation de réseaux sociaux</p>
+				<p>Prise en charge d'agendas</p>
+				<p>Opérations de communication</p>
+				<p>Organisation de déplacements et évènements</p>
+				<p>Élaboration de plannings</p>
+				<p>Animation de réunions, foires et séminaires</p>
+				<p>Procédures de recrutement</p>
+				<p>Montage de dossier de réponses à appels d'offres</p>
+				<p>Restructuration informatique</p>
+			</div>
+		</section>
+
+		<section id="outils">
+			<img class="background" src="img/outils.jpg" alt="outils de bureau">
+			<img class="background-responsive" src="img/outils_all.jpg" alt="outils de bureau">
+			<div id="outils-content">
+				<h2>OUTILS</h2>
+				<div class="hyphen"></div>
+				<div id="first-line">
+					<div>
+						<img src="img/svg/word.svg" alt="icone word">
+						<p>WORD</p>
+					</div>
+					<div>
+						<img src="img/svg/powerpoint.svg" alt="icone powerpoint">
+						<p>POWERPOINT</p>
+					</div>
+					<div>
+						<img src="img/svg/excel.svg" alt="icone excel">
+						<p>EXCEL</p>
+					</div>
+					<div>
+						<img src="img/svg/outlook.svg" alt="icone outlook">
+						<p>OUTLOOK</p>
+					</div>
+					<div>
+						<img src="img/svg/publisher.svg" alt="icone publisher">
+						<p>PUBLISHER</p>
+					</div>
+				</div>
+				<div id="second-line">
+					<div>
+						<img src="img/svg/paint.svg" alt="icone paint">
+						<p>PAINT</p>
+					</div>
+					<div>
+						<img src="img/svg/internet.svg" alt="icone internet">
+						<p>INTERNET</p>
+					</div>
+					<div>
+						<img src="img/svg/chorus.svg" alt="icone chorus">
+						<p>CHORUS</p>
+					</div>
+					<div>
+						<img src="img/svg/gmail.svg" alt="icone gmail">
+						<p>GMAIL</p>
+					</div>
+					<div>
+						<img src="img/svg/go7.svg" alt="icone go7">
+						<p>GO7</p>
+					</div>
+					<div>
+						<img src="img/svg/mfg.svg" alt="icone mfg">
+						<p>MFG PRO</p>
+					</div>
+				</div>
+
+				<div id="outils-responsive">
+					<div><img src="img/svg/word.svg" alt="icone word">
+						<p>WORD</p>
+					</div>
+					<div><img src="img/svg/powerpoint.svg" alt="icone powerpoint">
+						<p>POWERPOINT</p>
+					</div>
+					<div><img src="img/svg/excel.svg" alt="icone excel">
+						<p>EXCEL</p>
+					</div>
+					<div><img src="img/svg/outlook.svg" alt="icone outlook">
+						<p>OUTLOOK</p>
+					</div>
+					<div><img src="img/svg/publisher.svg" alt="icone publisher">
+						<p>PUBLISHER</p>
+					</div>
+					<div><img src="img/svg/paint.svg" alt="icone paint">
+						<p>PAINT</p>
+					</div>
+					<div><img src="img/svg/internet.svg" alt="icone internet">
+						<p>INTERNET</p>
+					</div>
+					<div><img src="img/svg/chorus.svg" alt="icone chorus">
+						<p>CHORUS</p>
+					</div>
+					<div><img src="img/svg/gmail.svg" alt="icone gmail">
+						<p>GMAIL</p>
+					</div>
+					<div><img src="img/svg/go7.svg" alt="icone go7">
+						<p>GO7</p>
+					</div>
+					<div><img src="img/svg/mfg.svg" alt="icone mfg">
+						<p>MFG PRO</p></div>
+					</div>
+				</div>
+			</section>
+
+			<section id="tarifs">
+				<div id="header-tarifs">
+					<h2>TARIFS</h2>
+					<div class="hyphen"></div>
+					<p class="header-tarifs-p">Des prestations en fonction de vos besoins</p>
+					<p class="header-tarifs-p">Toute demande de contact sera traitée dans les 24 heures</p>
+
+					<p id="header-tarifs-responsive">Des prestations en fonction de vos besoins. Toute demande de contact sera traitée dans les 24 heures</p>
+				</div>
+				<img id="plan" src="img/plan.jpg" alt="photo d'un planning">
+				<div id="bloc-couleur-quatre"></div>
+
+				<div class="bloc-texte" id="teletravail">
+					<h3>Télétravail</h3>
+					<div class="hyphen"></div>
+					<p>- 1/2 journée (4h) : 80€</p>
+					<p>- 1 journée (8h) : 150€</p>
+				</div>
+
+				<div class="bloc-texte" id="travailsursite">
+					<h3>Travail sur site</h3>
+					<div class="hyphen"></div>
+					<p>(dans un rayon de 25kms autour de Cahors,</p>
+					<p>au-delà, me consulter)</p>
+					<p>- 1/2 journée (4h) : 110€</p>
+					<p>- 1 journée (8h) : 200€</p>
+				</div>
+
+				<img id="desk" src="img/desk.jpg" alt="bureau et outils de bureau">
+				<div id="bloc-couleur-cinq"></div>
+
+				<div class="bloc-texte" id="forfait">
+					<h3>Forfait horaire</h3>
+					<div class="hyphen"></div>
+					<p>(valable 3 mois, à utiliser selon demande)</p>
+					<p>- 10h en télétravail : 210€</p>
+					<p>- 10h sur site : 290€</p>
+					<p>(dans un rayon de 25kms autour de Cahors)</p>
+				</div>
+				<div id="tarifs-footer">
+					<p>Toute mission ne pourra débuter qu'après signature d'un devis</p>
+					<p>Le paiement s'exécute à réception de facture pas chèque ou par virement bancaire</p>
+					<p>La facturation s'effectue à la fin de chaque mission ponctuelle ou selon un échéancier mensuel</p>
+					<p>en cas de missions régulières</p>
+					<div class="hyphen"></div>
+					<p>Le forfait horaire est payable d'avance</p>
+					<div class="hyphen"></div>
+					<p>Entreprise non asujettie à la TVA (art. 293B du CGI)</p>
+				</div>
+				<div id="tarifs-footer-responsive">
+					<p>Toute mission ne pourra débuter qu'après signature d'un devis. Le paiement s'exécute à réception de facture pas chèque ou par virement bancaire. La facturation s'effectue à la fin de chaque mission ponctuelle ou selon un échéancier mensuel en cas de missions régulières. Le forfait horaire est payable d'avance. Entreprise non asujettie à la TVA (art. 293B du CGI)</p>
+				</div>
+			</section>
+
+			<section id="contact">
+				<h2>CONTACT</h2>
+				<div class="hyphen"></div>
+				<form method="post">
+					<div id="contact-input">
+						<div id="inputs-left">
+							<input type="text" name="nom" placeholder="NOM" required>
+							<input type="email" name="email" placeholder="EMAIL" required>
+							<input type="text" name="sujet" placeholder="SUJET" required>
+						</div>
+						<textarea name="message" placeholder="MESSAGE" required></textarea>
+					</div>
+					<input id="contact-envoyer" type="submit" name="envoyer" value="ENVOYER">
+				</form>
+				<?php
+				if (isset($_POST['message'])) {
+					$position_arobase = strpos($_POST['email'], '@');
+					if ($position_arobase === false)
+						echo '<p>Votre email doit comporter un arobase.</p>';
+					else {
+						$retour = mail('sophie.quehen@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From: ' . $_POST['email']);
+						if($retour)
+							echo '<p>Votre message a bien été envoyé</p>';
+						else
+							error_reporting(E_ALL);
+						echo "<p>Erreur lors de l'envoi du message</p>";
+					}
+				}
+				?>
+			</section>
+		</div>
+
+		<footer>
+			<p>Perrine Quehen</p>
+			<div class="hyphen"></div>
+			<p>06 06 58 77 25</p>
+			<div class="hyphen"></div>
+			<p>Liberty.time46@gmail.com</p>
+			<div class="hyphen"></div>
+			<p>Mentions légales</p>
+			<div class="hyphen"></div>
+			<p>©2019</p>
+		</footer>
+
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript">
+	//pour le menu burger
+	$('.burger').click(function(){
+		$('.content').css( 'display', 'none' );
+		$('#page-burger').css( 'display', 'flex' );
+		$('footer').css( 'display', 'none' );
+		$('.sticky-menu').css('display', 'none');
+		$('.sticky-close').css('display', 'block');		
+	});
+
+	$('.sticky-close').click(function(){
+		$('.content').css( 'display', 'block' );
+		$('#page-burger').css( 'display', 'none' );
+		$('footer').css( 'display', 'flex' );
+		$('.sticky-menu').css('display', 'block');
+		$('.sticky-close').css('display', 'none');
+	});
+	//pour les liens burger
+	$('.liens-burger').click(function(){	
+		$('.content').css( 'display', 'block' );
+		$('#page-burger').css( 'display', 'none' );
+		$('footer').css( 'display', 'flex' );
+		$('.sticky-menu').css('display', 'block');
+		$('.sticky-close').css('display', 'none');
+	});
+
+</script>
+<script type="text/javascript">
+//sticky header
+$( document ).ready(function() {
+	$(window).scroll(function(){
+		var sticky = $('#sticky-header'),
+		scroll = $(window).scrollTop();
+
+		if (scroll >= 150) {
+			sticky.addClass('fixed');
+			sticky.css( "display", "flex" );
+		}else{ 
+			sticky.removeClass('fixed');
+			sticky.css( "display", "none" );
+		}
+	});
+});
+</script>
+<script type="text/javascript">
+	// Opera 8.0+
+	var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+    // Firefox 1.0+
+    var isFirefox = typeof InstallTrigger !== 'undefined';
+    // At least Safari 3+: "[object HTMLElementConstructor]"
+    var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+    // Internet Explorer 6-11
+    var isIE = /*@cc_on!@*/false || !!document.documentMode;
+    // Edge 20+
+    var isEdge = !isIE && !!window.StyleMedia;
+    // Chrome 1+
+    var isChrome = !!window.chrome && !!window.chrome.webstore;
+    // Blink engine detection
+    var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+    if(isIE){
+    	$('.cqui').css('display', 'none');
+    	$('ul').css('display', 'none');
+    	$('a').css('display', 'none');
+    	$('.container').css('display', 'none');
+    	$('.footer').css('display', 'none');
+    }else{
+    	$('.navigateur').css('display', 'none')
+    };
+</script>
+</body>
+</html>
